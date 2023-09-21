95117a7ea0a94262648efcfecc842d6891086844
MADMAX110 <2358598825@qq.com>
2023-09-22 00:28:21 +0800

    third commit

--- app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
@@ -6,6 +6,10 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -20,5 +24,21 @@ public class OrderActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         //启用向上按钮，尽管我们要用一个工具条作为应用条
         actionBar.setDefaultDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClickDone(View view){
+        CharSequence text = "Your order has been updated";
+        //让其显示很短的时间
+        int duration = Snackbar.LENGTH_SHORT;
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator), text, duration);
+        snackbar.setAction("Undo", new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(OrderActivity.this, "Undone!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+        snackbar.show();
     }
 }
